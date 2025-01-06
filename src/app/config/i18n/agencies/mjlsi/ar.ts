9f1e072b0fc872d73a027b64150d28cb513c01fd
Hazem Safwat <striker.h@gmail.com>
2025-01-06 12:40:27 +0300

    deploy 1

--- src/app/config/i18n/agencies/mjlsi/ar.ts
+++ src/app/config/i18n/agencies/mjlsi/ar.ts
@@ -1,29 +1,81 @@
 export const locale = {
-    lang:"ar",
-    data: {
-        AUTH: {
-            LOGIN: {
-                REGISTER_DESC: 'سجل منشأتك لطلب اشتراك نظام مجلسي Mjlsi'
-            },
-            REGISTER: {
-                TITLE: 'سجل لإستخدام نظام مجلسي Mjlsi',
-            }
-        },
-        WELCOME_SPLACH: {
-            TITLE: 'مجلسي: تعاون أفضل داخل الاجتماعات',
+	lang: 'ar',
+	data: {
+		AUTH: {
+			LOGIN: {
+				REGISTER_DESC: 'سجل منشأتك لطلب اشتراك نظام مجلسي Mjlsi'
+			},
+			REGISTER: {
+				TITLE: 'سجل لإستخدام نظام مجلسي Mjlsi',
+			}
+		},
+		WELCOME_SPLACH: {
+			TITLE: 'مجلسي: تعاون أفضل داخل الاجتماعات',
 			WELCOME: 'مرحبا بك فى مجلسي',
-            LONG_DESC:
+			LONG_DESC:
 				'مجلسي هو نظام يقوم بإنشاء وإدارة اجتماعات مجالس الإدارة في جميع المراحل من خلال التحضير للاجتماع ومناقشة الموضوعات والوثائق والعرض التقديمي والمشاركة واتخاذ القرار وتعيين المهام وإعداد محاضر الاجتماع',
-        },
-        SETTINGS : {
-            system_before_meeting_video_url: 'رابط فيديو نظام مجلسي - ماقبل الأجتماع',
-            explain_create_meeting_video_url:
+		},
+		SETTINGS: {
+			system_before_meeting_video_url: 'رابط فيديو نظام مجلسي - ماقبل الأجتماع',
+			explain_create_meeting_video_url:
 				'رابط فيديو شرح انشاء اجتماع نظام مجلسي',
-            manage_board_meeting_video_url:
+			manage_board_meeting_video_url:
 				'رابط فيديو نظام مجلسي لإدارة أجتماعات مجالس الادارات',
 			manage_board_meeting_extra_video_url:
 				'رابط فيديو نظام مجلسي لإدارة أجتماعات مجالس الادارات',
-        },
-        COMPANY_NAME: "Mjlsi",
-    }
-}
+		},
+		COMPANY_NAME: 'Mjlsi',
+		COMMITTEES: {
+			ADD: {
+				COMMITTEEORGANISER: 'أمين مجلس اللجنة *',
+				COMMITTEE_ORGANISER: 'أمين مجلس اللجنة',
+				COMMITTEE_ORGANISER_ERROR: 'أمين مجلس اللجنة غير موجود',
+			},
+			VALIDATION: {
+				COMMITTEEORGANISER: 'أمين مجلس اللجنة مطلوب',
+			}
+		},
+		MEETINGS: {
+			GENERAL: {
+				ORGANISERS: 'أمين اللجنة',
+			},
+			LIST: {
+				MEETINGCOMMITTEE: 'اللجنة',
+			},
+			INFO: {
+				ADD: {
+					MEETINGCOMMITTEE: 'اللجنة',
+				},
+				VALIDATION: {
+					MEETINGCOMMITTEE: 'اللجنة مطلوبة.',
+				}
+			},
+			ORGANISERS: {
+				ADD: {
+					ADDORGANISER: 'إضافة أمين اللجنة',
+					ADDMEMBER: 'إضافة امين اللجنة',
+					MEETINGORGANISERS: 'أمين اللجنة',
+					NORECORDSFOUND: 'لم يتم العثور على أمين اللجنة',
+					MEETINGORGANISER: 'أمين اللجنة',
+				},
+				VALIDATION: {
+					MEETINGORGANISERS: 'أمناء اللجان مطلوبين',
+					MEETINGORGANISER: 'أمين اللجنة مطلوب',
+				},
+			}
+		},
+		MEETINGS_DASHBOARD: {
+			LIST: {
+				COMMITTEES: 'اللجنة',
+			},
+		},
+		VIEW_MEETING: {
+			ORGANIZATIONS: 'أمين اللجنة',
+		},
+		APPROVAL: {
+			LIST: {
+				COMMITTEES: 'اللجنة',
+			},
+		}
+	}
+};
